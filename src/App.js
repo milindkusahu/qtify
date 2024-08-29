ba9bdae9fb4ae4cdb2ca0c6508b5b991f609ab95
milindkusahu <milindsahu2000@gmail.com>
2024-08-29 12:45:11 +0530

    Project Completed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
+import React, { useEffect, useState } from "react";
+import { Outlet } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
-import Hero from "./components/Hero/Hero";
+import { StyledEngineProvider } from "@mui/material";
+import { fetchTopAlbums, fetchNewAlbums, fetchSongs } from "./api/api";
 
 function App() {
+  const [data, setData] = useState({});
+
+  const generateData = (key, source) => {
+    source().then((data) => {
+      setData((prevState) => {
+        return { ...prevState, [key]: data };
+      });
+    });
+  };
+
+  useEffect(() => {
+    generateData("topAlbums", fetchTopAlbums);
+    generateData("newAlbums", fetchNewAlbums);
+    generateData("songs", fetchSongs);
+  }, []);
+
+  const { topAlbums = [], newAlbums = [], songs = [] } = data;
+
   return (
-    <div>
-      <Navbar />
-      <Hero />
-    </div>
+    <>
+      <StyledEngineProvider injectFirst>
+        <Navbar />
+        <Outlet context={{ data: { topAlbums, newAlbums, songs } }} />
+      </StyledEngineProvider>
+    </>
   );
 }
 
